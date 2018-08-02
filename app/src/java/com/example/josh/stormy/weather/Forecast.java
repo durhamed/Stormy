126b61c08b93af3832e7d7797fc32dbb782eab06
Josh <31577539+durhamed@users.noreply.github.com>
2018-08-02 00:19:44 -0400

    Add files via upload

---
+++ app/src/java/com/example/josh/stormy/weather/Forecast.java
@@ -0,0 +1,34 @@
+package com.example.josh.stormy.weather;
+
+/**
+ * Created by Josh on 8/21/2015.
+ */
+public class Forecast {
+    private Current mCurrent;
+    private Hour[] mHourlyForecast;
+    private Day[] mDailyForecast;
+
+    public Current getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(Current current) {
+        mCurrent = current;
+    }
+
+    public Hour[] getHourlyForecast() {
+        return mHourlyForecast;
+    }
+
+    public void setHourlyForecast(Hour[] hourlyForecast) {
+        mHourlyForecast = hourlyForecast;
+    }
+
+    public Day[] getDailyForecast() {
+        return mDailyForecast;
+    }
+
+    public void setDailyForecast(Day[] dailyForecast) {
+        mDailyForecast = dailyForecast;
+    }
+}
